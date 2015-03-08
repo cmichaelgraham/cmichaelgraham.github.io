a3d3474d8f027cb50420f1e8e6750d7b3dd18ead
Mike Graham <mgraham@technicalmedia.com>
2015-03-08 12:50:03 -0600

    added code-sandbox to deployment

---
+++ aurelia-typescript/code-sandbox/views/wiz/step-one.js
@@ -0,0 +1,10 @@
+define(["require", "exports"], function (require, exports) {
+    var StepOne = (function () {
+        function StepOne() {
+            var i = "hello";
+        }
+        return StepOne;
+    })();
+    exports.StepOne = StepOne;
+});
+//# sourceMappingURL=step-one.js.map
---
+++ aurelia-typescript/code-sandbox/views/wiz/step-one.ts
@@ -0,0 +1,5 @@
+﻿export class StepOne {
+    constructor() {
+        var i = "hello";
+    }
+} 
