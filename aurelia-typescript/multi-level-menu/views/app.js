f1f35ddb3e76633f8b220c9740bd421e2eeb7b4f
Mike Graham <mgraham@technicalmedia.com>
2015-03-10 21:00:30 -0600

    published multi-level-menu

--- aurelia-typescript/multi-level-menu/views/app.js
+++ aurelia-typescript/multi-level-menu/views/app.js
@@ -1,21 +1,12 @@
-define(["require", "exports", "aurelia-router", "./MultiLevelMenuPipelineStep"], function (require, exports, aur, mlmps) {
+define(["require", "exports", "aurelia-router"], function (require, exports, aur) {
+    // import t = require("./todos");
     var App = (function () {
         function App(router) {
             this.router = router;
             this.router.configure(function (config) {
-                config.title = "Aurelia VS/TS";
-                config.addPipelineStep("modelbind", mlmps.MultiLevelMenuPipelineStep);
+                config.title = "TodoMVC";
                 config.map([
-                    { route: ["", "home"], moduleId: "views/home", nav: true, title: "home", settings: { level: 0, show: true } },
-                    { route: ["item-1"], moduleId: "views/item-1", nav: true, title: "item 1", settings: { level: 0, show: true } },
-                    { route: ["item-1-1"], moduleId: "views/item-1-1", nav: true, title: "item 1.1", settings: { level: 1, show: false } },
-                    { route: ["item-1-2"], moduleId: "views/item-1-2", nav: true, title: "item 1.2", settings: { level: 1, show: false } },
-                    { route: ["item-2"], moduleId: "views/item-2", nav: true, title: "item 2", settings: { level: 0, show: true } },
-                    { route: ["item-2-1"], moduleId: "views/item-2-1", nav: true, title: "item 2.1", settings: { level: 1, show: false } },
-                    { route: ["item-2-2"], moduleId: "views/item-2-2", nav: true, title: "item 2.2", settings: { level: 1, show: false } },
-                    { route: ["item-2-2-1"], moduleId: "views/item-2-2-1", nav: true, title: "item 2.2.1", settings: { level: 2, show: false } },
-                    { route: ["item-2-2-2"], moduleId: "views/item-2-2-2", nav: true, title: "item 2.2.2", settings: { level: 2, show: false } },
-                    { route: ["item-2-3"], moduleId: "views/item-2-3", nav: true, title: "item 2.3", settings: { level: 1, show: false } }
+                    { route: ["", ":filter"], moduleId: "views/todos", nav: true }
                 ]);
             });
         }
