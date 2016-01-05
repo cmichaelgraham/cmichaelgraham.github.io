3a54492a826d2398795a030df7bb1ca53e072c51
Graham <ZIK51127@ihs.internal.corp>
2016-01-05 05:34:08 -0700

    add ng-ui-grid-flexbox sample

---
+++ ng-ui-grid-flexbox/content/grid-viewmodel.js
@@ -0,0 +1,32 @@
+﻿(function (module) {
+
+    module.controller('gridViewModel', [function () {
+
+        var gridData = [
+            { LastName: 'Graham', FirstName: 'Mike', FavoriteColor: 'green', GitHubID: 'cmichaelgraham' },
+            { LastName: 'Vader', FirstName: 'Darth', FavoriteColor: 'black', GitHubID: 'darthvader' },
+            { LastName: 'Solo', FirstName: 'Han', FavoriteColor: 'white', GitHubID: 'hansolo' },
+            { LastName: 'Hut', FirstName: 'Jabba', FavoriteColor: 'brown', GitHubID: 'jabbathehut' }
+        ];
+
+        var gridOptions = {
+            data: 'vm.gridData',
+            enableGridMenu: true,
+            columnDefs: [
+                { name: 'LastName', field: 'LastName', width: '25%' },
+                { name: 'FirstName', field: 'FirstName', width: '25%' },
+                { name: 'FavoriteColor', field: 'FavoriteColor', width: '25%' },
+                { name: 'GitHubID', field: 'GitHubID', width: '25%' }
+            ]
+        };
+
+        var o = {
+            message: 'this is the grid-viewmodel message...',
+            gridData: gridData,
+            gridOptions: gridOptions
+        };
+
+        return o;
+    }]);
+
+}(angular.module('app')));
