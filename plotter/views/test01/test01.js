da7e8c573913eec103a83217505aeb09379e0389
Mike Graham <mgraham@technicalmedia.com>
2016-07-22 20:16:22 -0600

    Update test01.js

--- plotter/views/test01/test01.js
+++ plotter/views/test01/test01.js
@@ -1,7 +1,9 @@
-require(["exports"], function (exports) {
+define('http://cmichaelgraham.github.io/plotter/views/test01/test01.js',["require", "exports"], function (require, exports) {
+    "use strict";
     var Test01 = (function () {
-        function Test01() {};
+        function Test01() {
+        }
         return Test01;
-    })();
+    }());
     exports.Test01 = Test01;
 });
