b1727fabd07a959545ea8b136c5d849cde9b5704
Mike Graham <mgraham@technicalmedia.com>
2016-07-22 19:54:55 -0600

    Update test01.html.js

--- plotter/views/test01/test01.html.js
+++ plotter/views/test01/test01.html.js
@@ -1,1 +1,5 @@
-define(['module'], function(module) { module.exports = "<template>\r\n  <h1>test01</h1>\r\n</template>"; });
+define('text!http://cmichaelgraham.github.io/plotter/views/test01/test01.html', 
+  ['module'], 
+  function(module) { 
+    module.exports = "<template>\r\n  <h1>test01</h1>\r\n</template>";
+  });
