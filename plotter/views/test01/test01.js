a1f64c22cea409392b1ed691dfbda2b063e457f1
Mike Graham <mgraham@technicalmedia.com>
2016-07-23 11:44:30 -0600

    Update test01.js

--- plotter/views/test01/test01.js
+++ plotter/views/test01/test01.js
@@ -2,6 +2,7 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     var Test01 = (function () {
         function Test01() {
+            this.message = 'Hello World, from Test01 on GitHub !!! :)';
         }
         return Test01;
     }());
